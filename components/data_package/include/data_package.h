0f67d90a9ea16db564827f03198176228f41716a
LPT0317 <75077760+LPT0317@users.noreply.github.com>
2023-11-26 09:05:19 +0700

    update header

--- components/data_package/include/data_package.h
+++ components/data_package/include/data_package.h
@@ -3,8 +3,8 @@
 #define RESPONSE 3
 
 void printData(char *data);
-void heartbeatPackage(uint8_t ID, int status, int signal, char* data);
-void dataPackage(uint8_t ID, int capacity, int floor, int trash, char* data);
+void heartbeatPackage(uint8_t ID, int status, int floor, int trash, int signal, char* data);
+void dataPackage(uint8_t ID, int capacity, char* data);
 void responsePackage(uint8_t ID, int type, char* data);
 void getData(const char* logName, char *data, char* json);
 
